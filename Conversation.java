674f34cd095adc934165d95ac2f3adc000c7015c
jen927 <157835344+jen927@users.noreply.github.com>
2024-02-16 18:09:16 -0500

    fixed the capitalization of the first word of a sentence.

--- Conversation.java
+++ Conversation.java
@@ -11,32 +11,54 @@ class Conversation {
     for (int i = 0; i < rounds; i++) { // loops for every user response (rounds)
 
       String userInput = sc.nextLine(); // user's response
-      
-      if (userInput.contains("you") || userInput.contains("I") || userInput.contains("me") ||
+      userInput = userInput.toLowerCase();
+      if (userInput.contains("you") || userInput.contains("i") || userInput.contains("me") ||
       userInput.contains("am")|| userInput.contains("my") || userInput.contains("your")) { //checks if there are any of the keywords in the user's response.
         // changes words: 
-        String[] splitInput = userInput.split(" "); // splits the response into an string array
-        for (int i = 0; 0 < splitInput.length; i++) { // will go through each word in the phrase.
-          String response = ""; // intial bot response
-          if (splitInput[i].equals("I")) { //replaces I with you
-          splitInput[i] = "you";
+        String[] splitInput = userInput.split("\\s+"); // splits the response into an string array
+      
+        StringBuilder response = new StringBuilder(""); // intial bot response
+        for (int a = 0; a < (splitInput.length) ; a++) { // will go through each word in the phrase.
+          if (splitInput[a].equals("i")) { //replaces I with you
+          splitInput[a] = "you";
+          response.append(splitInput[a]);
           }
-          else if (splitInput[i].equals("me")) { //replaces me with you
+          else if (splitInput[a].equals("me")) { //replaces me with you
             splitInput[i] = "you";
+            response.append(splitInput[a]); //adds word into bot response
           }
-          else if (splitInput[i].equals("am")) { // replaces am with are
-            splitInput[i] = "are";
+          else if (splitInput[a].equals("am")) { // replaces am with are
+            splitInput[a] = "are";
+            response.append(splitInput[a]);
           }
-          else if (splitInput[i].equals("you")) { // replaces you with I
-            splitInput[i] = "I";
+          else if (splitInput[a].equals("you")) { // replaces you with I
+            splitInput[a] = "I";
+            response.append(splitInput[a]);
           }
-          else if (splitInput[i].equals("my")) { //replaces my with your
-            splitInput[i] = "your";
+          else if (splitInput[a].equals("my")) { //replaces my with your
+            splitInput[a] = "your";
+            response.append(splitInput[a]);
           }
-          else if (splitInput[i].equals("your")) { //replaces your with my
-            splitInput[i] = "my";
+          else if (splitInput[a].equals("your")) { //replaces your with my
+            splitInput[a] = "my";
+            response.append(splitInput[a]);
           }
-        }    
+          else {
+            response.append(splitInput[a]);
+          }  
+        }
+        
+        String sent = response.toString().substring(0,1).toUpperCase() + response.toString().substring(1);
+        
+        if (sent.contains(".")){
+          String newSent = sent.replace(".","?");
+          System.out.println(newSent);
+        }
+        else {
+          System.out.println(sent);
+        }
+
+          
       }
       else {
         // create array of phrases the bot can respond with
